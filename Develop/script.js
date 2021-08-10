78744b5621d3e77de7ccf1ef718d955a3976ddac
JoseEPina <jose.edpina@gmail.com>
2021-08-09 20:56:17 -0600

    Validated all Criteria and Validated user input to include at least One criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,144 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const CRITERIA_NUMBER = 4;
+const NUMBER_OF_CRITERIA = 4;
 
+// Criteria:
+// name - Criteria Name
+// selected - was this criteria selected for Pwd? (Yes/No)
+// totalCharsInPwd - random # of total criteria chars TO BE used in Pwd
+// assignedCharsInPwd - current number of criteria chars used in Pwd
+// setOfChars - define set of chars for this criteria
 var criteria = [
+   {
+      name: "lower-case", // Criteria Name
+      selected: false, // Criteria Selected Yes/No
+      totalCharsInPwd: 0, // Random# of Criteria Chars used in Pwd
+      assignedCharsInPwd: 0, // Actual Criteria Chars used in Pwd
+      setOfChars: [
+         "a",
+         "b",
+         "c",
+         "d",
+         "e",
+         "f",
+         "g",
+         "h",
+         "i",
+         "j",
+         "k",
+         "l",
+         "m",
+         "n",
+         "o",
+         "p",
+         "q",
+         "r",
+         "s",
+         "t",
+         "u",
+         "v",
+         "w",
+         "x",
+         "y",
+         "z",
+      ],
+   },
    {
       name: "upper-case",
       selected: false,
-   },
-   {
-      name: "lower-case",
-      selected: false,
+      totalCharsInPwd: 0,
+      assignedCharsInPwd: 0,
+      setOfChars: [
+         "A",
+         "B",
+         "C",
+         "D",
+         "E",
+         "F",
+         "G",
+         "H",
+         "I",
+         "J",
+         "K",
+         "L",
+         "M",
+         "N",
+         "O",
+         "P",
+         "Q",
+         "R",
+         "S",
+         "T",
+         "U",
+         "V",
+         "W",
+         "X",
+         "Y",
+         "Z",
+      ],
    },
    {
       name: "special-characters",
       selected: false,
+      totalCharsInPwd: 0,
+      assignedCharsInPwd: 0,
+      setOfChars: [
+         "!",
+         "(",
+         ")",
+         "-",
+         ".",
+         "?",
+         "[",
+         "]",
+         "_",
+         "`",
+         "~",
+         ";",
+         ":",
+         "@",
+         "#",
+         "$",
+         "%",
+         "^",
+         "&",
+         "*",
+         "+",
+         "=",
+      ],
    },
    {
       name: "numeric",
       selected: false,
+      totalCharsInPwd: 0,
+      assignedCharsInPwd: 0,
+      setOfChars: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
    },
 ];
 
 // Presents prompts in a Loop
 var presentPrompts = function () {
    console.log("Inside presentPrompts");
-   debugger;
-   for (var i = 0; i < CRITERIA_NUMBER; i++) {
+   // debugger;
+   // const value of True to use for
+   const YES = true;
+   // Assumption that user did not select any Criteria
+   var noneSelected = true;
+
+   // Loops to present All criteria
+   for (var i = 0; i < NUMBER_OF_CRITERIA; i++) {
+      // Receive user input (Yes/True or No/Fasle) on property 'selected' of object Criteria
       criteria[i].selected = window.confirm("Do you want to use " + criteria[i].name + " characters in the password?");
+      // If at least one user input is Yes/True, then noneSelected assumption is False.
+      if (criteria[i].selected == YES) {
+         noneSelected = false;
+      }
+   }
+   // If assumption is True (and No Criteria was selected), then send an alert message and prompt again.
+   if (noneSelected == true) {
+      window.alert("Please select at least ONE valid criteria. Try again.");
+      presentPrompts();
    }
 };
 
@@ -54,6 +165,7 @@ function generatePassword() {
    console.log("Inside generatePassword");
    presentPrompts();
    var passwordLength = promptPasswordLength();
+   var assignPwdLengthToCriteriaLenght = function (passwordLength) {};
 }
 
 // Write password to the #password input
