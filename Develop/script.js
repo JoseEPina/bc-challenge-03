4875bcaac1e0d1d4f22b7fc6503ef3276a57679b
JoseEPina <jose.edpina@gmail.com>
2021-08-11 00:45:49 -0600

    Added algorith to randomly select a character from a randomly selected character set

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,8 @@ const NUMBER_OF_CRITERIA = 4;
 // const value of True to validate user 'selected' criteria
 const YES = true;
 
+var passwordLength = 0;
+
 // Criteria Object:
 // name - Criteria Name
 // selected - was this criteria selected for Pwd? (Yes/No)
@@ -45,6 +47,7 @@ var criteria = [
          "y",
          "z",
       ],
+      setLength: 26,
    },
    {
       name: "upper-case",
@@ -79,6 +82,7 @@ var criteria = [
          "Y",
          "Z",
       ],
+      setLength: 26,
    },
    {
       name: "special-characters",
@@ -109,6 +113,7 @@ var criteria = [
          "+",
          "=",
       ],
+      setLength: 22,
    },
    {
       name: "numeric",
@@ -116,6 +121,7 @@ var criteria = [
       totalCharsInPwd: 0,
       assignedCharsInPwd: 0,
       setOfChars: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+      setLength: 10,
    },
 ];
 
@@ -166,43 +172,77 @@ var promptPasswordLength = function () {
    return passwordLength;
 };
 
+var calculateCriteriaLength = function () {
+   // Determine count of selected criteria
+   var countedSelectedCriteria = 0;
+   for (var i = 0; i < NUMBER_OF_CRITERIA; i++) {
+      if (criteria[i].selected === YES) {
+         countedSelectedCriteria++;
+      }
+   }
+
+   // Determine each Criteria Characters' group size
+   var equalSize = Math.floor(passwordLength / countedSelectedCriteria);
+   // Adjust criteria size of password Length to compensate for Math.floor
+   var adjustedSize = passwordLength - equalSize * (countedSelectedCriteria - 1);
+
+   // Controls processing of groups with equal and different size of characters (due to use of Math.floor)
+   var auxIndex = 1;
+   // Loop to process object criteria array and assign max number of random characters per criteria selected
+   for (var i = 0; i < NUMBER_OF_CRITERIA; i++) {
+      // Process only if criteria was selected.
+      if (criteria[i].selected == YES) {
+         // Process all criteria except last one.
+         if (auxIndex < countedSelectedCriteria) {
+            criteria[i].totalCharsInPwd = equalSize;
+         } else {
+            // Process last criteria.
+            criteria[i].totalCharsInPwd = adjustedSize;
+         }
+         auxIndex++;
+      }
+   }
+};
+
+var buildPassword = function () {
+   // initialize local password variable.
+   var password = "";
+   // Loop to begin 'building' Pwd, ONE character at a time until we reach total PwdLength.
+   for (var i = 0; i < passwordLength; i++) {
+      // Randomly select one of the user's Criteria. This will ensure that we don't USE all the characters of ONE Criteria all together at once.
+      var randomSet = Math.floor(Math.random() * (countedSelectedCriteria - 1)) + 1;
+
+      // Local auxiliary index; To help find the matching Criteria array index position that corresponds with the randomSet.
+      var auxIndex = 0;
+      // Loops through Criteria array object and validate if index position corresponds with randomSet.
+      for (var j = 0; j < NUMBER_OF_CRITERIA; j++) {
+         if (criteria[j].selected === YES) {
+            auxIndex++; // Increment auxIndex for each selected criteria
+         }
+
+         // Check for condition When array index position corresponds with the randomSet.
+         if (auxIndex === randomSet) {
+            // Randomly select the index of a character within the (randomSet) criteria's setOfChars.
+            var charIndex = Math.floor(Math.random() * (criteria[j].setLength - 1)) + 1;
+            // Select a random character from random selected Criteria.
+            var character = criteria[j].setOfChars[charIndex];
+            // concatenate character to password.
+            password += character;
+            console.log("exiting If block with break");
+            // Breaks current loop and moves to process next password's character.
+            break;
+         }
+         console.log("finish Loop j");
+      }
+   }
+};
+
 function generatePassword() {
    console.log("Inside generatePassword");
    presentPrompts();
-   var passwordLength = promptPasswordLength();
-   var calculateCriteriaLength = function (passwordLength) {
-      // Determine count of selected criteria
-      var countedSelectedCriteria = 0;
-      for (var i = 0; i < NUMBER_OF_CRITERIA; i++) {
-         if (criteria[i].selected === YES) {
-            countedSelectedCriteria++;
-         }
-      }
-
-      // Determine each Criteria Characters' group size
-      var equalSize = Math.floor(passwordLength / countedSelectedCriteria);
-      // Adjust criteria size of password Length to compensate for Math.floor
-      var adjustedSize = passwordLength - equalSize * (countedSelectedCriteria - 1);
-
-      // Controls processing of groups with equal and different size of characters (due to use of Math.floor)
-      var auxIndex = 1;
-      // Loop to process object criteria array and assign max number of random characters per criteria selected
-      for (var i = 0; i < NUMBER_OF_CRITERIA; i++) {
-         // Process only if criteria was selected.
-         if (criteria[i].selected == YES) {
-            // Process all criteria except last one.
-            if (auxIndex < countedSelectedCriteria) {
-               criteria[i].totalCharsInPwd = equalSize;
-            } else {
-               // Process last criteria.
-               criteria[i].totalCharsInPwd = adjustedSize;
-            }
-            auxIndex++;
-         }
-      }
-
-      var password = "";
-   };
+   passwordLength = promptPasswordLength();
+   calculateCriteriaLength();
+   buildPassword();
 }
 
 // Write password to the #password input
